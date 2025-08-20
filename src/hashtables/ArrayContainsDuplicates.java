717b794c5813654e3c90e2ff2ae453a108fee7ee
corina <valeriabalan1997@gmail.com>
2025-08-20 20:34:57 +0300

    hashset duplicates

---
+++ src/hashtables/ArrayContainsDuplicates.java
@@ -0,0 +1,23 @@
+package hashtables;
+
+import java.util.HashSet;
+
+public class ArrayContainsDuplicates {
+
+    public boolean containsDuplicate(int[] nums) {
+
+        HashSet<Integer> hashSet=new HashSet<>();
+        for(int num: nums){
+
+          if(hashSet.contains(num)){
+
+              return true;
+          }
+
+          hashSet.add(num);
+        }
+        return false;
+    }
+
+
+}
